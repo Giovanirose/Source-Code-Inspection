9803e75be4bede926213effc6a331532df752040
Giovani Castilho <gigiocastilho@hotmail.com>
2017-09-07 20:53:46 -0300

    Closes#1 Ticket Machine Feature 0.0.1

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -1,9 +1,12 @@
 package br.calebe.ticketmachine.exception;
-
 /**
- *
  * @author Calebe de Paula Bianchini
+ * @author Giovani Castilho
+ * @author Otavio Villela
  */
 public class PapelMoedaInvalidaException extends Exception {
-    
+
+    public PapelMoedaInvalidaException() {
+        super("Esta nota de papel moeda é inválida, tente novamente!");
+    }   
 }
