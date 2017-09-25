dbf1ea1348a54c0af9994d74487d4106357f921b
31683088 <31683088@093054-P31S405.macklabs.br>
2017-09-25 18:48:31 -0300

    Criação da classe main e correção do método inserir na classe TicketMachine

---
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/ProjetoES.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.calebe.ticketmachine.core;
+
+/**
+ * @author Calebe de Paula Bianchini
+ * @author Giovani Castilho
+ * @author Otavio Villela
+ */
+public class ProjetoES {
+    public static void main(String[] args) {
+        
+    }
+  
+}
