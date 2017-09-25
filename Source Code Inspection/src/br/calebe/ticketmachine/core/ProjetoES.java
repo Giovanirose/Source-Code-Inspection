01c8a2c7181d2101d781e591b3d78a1cc8e891e7
31683088 <31683088@093054-P31S405.macklabs.br>
2017-09-25 19:13:14 -0300

    Intsancia de Ticket Machine na main

--- Source Code Inspection/src/br/calebe/ticketmachine/core/ProjetoES.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/ProjetoES.java
@@ -1,4 +1,9 @@
 package br.calebe.ticketmachine.core;
+
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author Calebe de Paula Bianchini
  * @author Giovani Castilho
@@ -6,6 +11,24 @@ package br.calebe.ticketmachine.core;
  */
 public class ProjetoES {
     public static void main(String[] args) {
+        TicketMachine tm = new TicketMachine(0);
+        
+        // TESTES
+        tm.valor = 20;
+        tm.saldo = 1000;
+        int notaDeDois = tm.papelMoeda[0];
+        int notaDeCinco = tm.papelMoeda[1];
+        int notaDeDez = tm.papelMoeda[2];
+        int notaDeVinte = tm.papelMoeda[3];
+        int notaDeCinquenta = tm.papelMoeda[4];
+        int notaDeCem = tm.papelMoeda[5];
+        
+        try {
+            tm.inserir(40);
+        } catch (PapelMoedaInvalidaException ex) {
+            ex.getMessage();
+        }
+        
         
     } 
 }
