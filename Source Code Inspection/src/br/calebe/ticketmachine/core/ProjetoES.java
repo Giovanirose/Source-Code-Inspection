d00b158caf8024dfade9ee51a3773ccb7df47639
Giovani Castilho <gigiocastilho@hotmail.com>
2017-11-04 16:15:56 -0200

    Ticket Machine v1.1.1 Finalizada

--- Source Code Inspection/src/br/calebe/ticketmachine/core/ProjetoES.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/ProjetoES.java
@@ -1,8 +1,8 @@
 package br.calebe.ticketmachine.core;
 
 import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
+import java.util.Scanner;
 
 /**
  * @author Calebe de Paula Bianchini
@@ -13,29 +13,29 @@ import java.util.logging.Logger;
 public class ProjetoES {
 
     public static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SaldoInsuficienteException {
         TicketMachine tm = null; //Instância nula para facilitar testes e utilização
         Utilizar(tm); // Método para utilização da classe TicketMachine
         
         // --- TESTES --- //
-        tm.valor = 20;
-        tm.saldo = 1000;
-        int notaDeDois = tm.papelMoeda[0];
-        int notaDeCinco = tm.papelMoeda[1];
-        int notaDeDez = tm.papelMoeda[2];
-        int notaDeVinte = tm.papelMoeda[3];
-        int notaDeCinquenta = tm.papelMoeda[4];
-        int notaDeCem = tm.papelMoeda[5];
-        
-        try {
-            tm.inserir(40);
-        } catch (PapelMoedaInvalidaException ex) {
-            ex.getMessage();
-        }
+//        tm.valor = 20;
+//        tm.saldo = 1000;
+//        int notaDeDois = tm.papelMoeda[0];
+//        int notaDeCinco = tm.papelMoeda[1];
+//        int notaDeDez = tm.papelMoeda[2];
+//        int notaDeVinte = tm.papelMoeda[3];
+//        int notaDeCinquenta = tm.papelMoeda[4];
+//        int notaDeCem = tm.papelMoeda[5];
+//        
+//        try {
+//            tm.inserir(40);
+//        } catch (PapelMoedaInvalidaException ex) {
+//            ex.getMessage();
+//        }
         // --- FIM DOS TESTES --- //
     } 
     
-    public static void Utilizar(TicketMachine tm){
+    public static void Utilizar(TicketMachine tm) throws SaldoInsuficienteException{
         System.out.println("Digite a quantia a ser inserida: ");
             int quantia = Integer.parseInt(sc.nextLine());
         tm = new TicketMachine(quantia);
@@ -43,7 +43,7 @@ public class ProjetoES {
         try {
            tm.inserir(quantia);
            tm.getTroco();
-        } catch (PapelMoedaInvalidaException | SaldoInsuficienteException ex){
+        } catch (PapelMoedaInvalidaException ex){
            ex.getMessage();
         }
         tm.imprimir();
